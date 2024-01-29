47dbdf23ad75f5bf62e61c532989cd784126ec1d
Bojan Durdevic <bojan88@gmail.com>
2024-01-29 08:26:11 -0800

    changes for @azure/msal-browser upgrade to v3

--- src/Auth/msalInstance.ts
+++ src/Auth/msalInstance.ts
@@ -24,28 +24,32 @@ const msalInstance = new PublicClientApplication({
   },
 });
 
-// Default to using the first account if no account is active on page load
-if (
-  !msalInstance.getActiveAccount() &&
-  msalInstance.getAllAccounts().length > 0
-) {
-  msalInstance.setActiveAccount(
-    msalInstance
-      .getAllAccounts()
-      .find(
-        ({ environment, tenantId }) =>
-          environment === VITE_MSAL_ENVIRONMENT &&
-          tenantId === VITE_MSAL_TENANT_ID
-      ) ?? null
-  );
-}
+export async function initializeMsalInstance() {
+  await msalInstance.initialize();
 
-msalInstance.addEventCallback((event: EventMessage) => {
-  if (event.eventType === EventType.LOGIN_SUCCESS && event.payload) {
-    const payload = event.payload as AuthenticationResult;
-    const account = payload.account;
-    msalInstance.setActiveAccount(account);
+  // Default to using the first account if no account is active on page load
+  if (
+    !msalInstance.getActiveAccount() &&
+    msalInstance.getAllAccounts().length > 0
+  ) {
+    msalInstance.setActiveAccount(
+      msalInstance
+        .getAllAccounts()
+        .find(
+          ({ environment, tenantId }) =>
+            environment === VITE_MSAL_ENVIRONMENT &&
+            tenantId === VITE_MSAL_TENANT_ID
+        ) ?? null
+    );
   }
-});
+
+  msalInstance.addEventCallback((event: EventMessage) => {
+    if (event.eventType === EventType.LOGIN_SUCCESS && event.payload) {
+      const payload = event.payload as AuthenticationResult;
+      const account = payload.account;
+      msalInstance.setActiveAccount(account);
+    }
+  });
+}
 
 export default msalInstance;
